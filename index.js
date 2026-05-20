104054456f4d149425c149ed7d7e4e08ab66ad09
DemonMagicGhost <ajkhfauhfjkhufduhsjf@gmail.com>
2026-05-20 21:34:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -101,9 +101,11 @@ app.get("/files", (req, res) => {
 });
 
 // start server
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 
 app.delete("/delete/:id", async (req, res) => {
 
